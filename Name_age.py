f4fa3b2aed6aeb7716f88b3ec91795535fe602e2
Bart2314 <91088674+Bart2314@users.noreply.github.com>
2021-10-10 18:39:26 +0200

    my first repository

---
+++ Name_age.py
@@ -0,0 +1,4 @@
+name = input("Jak masz na imię? ")
+age = int(input("Ile masz lat? "))
+print( "Twoje imię to " + name + " i masz " + str(age) + " lat.")
+             
---
+++ dayrater.py
@@ -0,0 +1,2 @@
+rating = input("Oceń swój dzień w skali od 0 do 10: ")
+print("Twoja ocena: " + rating)
